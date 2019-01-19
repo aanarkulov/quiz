8b8941ad907d9c1cbf6354a5a9cd97dbe5455ac7
aanarkulov <a.anarkuloff@gmail.com>
2019-01-19 21:42:04 +0600

    Загрузка списка тестов

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -87,12 +87,6 @@ class QuizCreator extends Component {
         } catch (e) {
             console.log(e)
         }
-
-        // axios.post('https://quiz-2019a.firebaseio.com/quizes.json', this.state.quiz)
-        //     .then(response => {
-        //         console.log(response)
-        //     }).catch(error => console.log(error))
-
     }
 
     onChangeHandler = (value, controlName) => {
--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react'
 import classes from './QuizList.css'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
 class QuizList extends Component {
+    state = {
+        quizes: []
+    }
 
     renderQuiz() {
-        return [1, 2, 3].map((quiz, index) => {
+        return this.state.quizes.map((quiz) => {
             return (
                 <li
-                    key={index}
+                    key={quiz.id}
                 >
-                    <NavLink to={'/quiz/' + quiz}>
-                        Тест {quiz}
+                    <NavLink to={'/quiz/' + quiz.id}>
+                        {quiz.name}
                     </NavLink>
                 </li>
             )
         })
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get('https://quiz-2019a.firebaseio.com/quizes.json')
+
+            const quizes = []
+
+            Object.keys(response.data).forEach((key, index) => {
+                quizes.push({
+                    id: key,
+                    name: `Тест № ${index + 1}`
+                })
+            })
+
+            this.setState({
+                quizes
+            })
+
+        } catch (e) {
+
+        }
     }
 
     render() {
