87e26ae9630557a09fc353159b12dcdd75990f88
aanarkulov <a.anarkuloff@gmail.com>
2019-01-19 11:29:50 +0600

    Настройка роутера

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout';
-import Quiz from './containers/Quiz/Quiz';
+import React, { Component } from 'react'
+import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz/Quiz'
+
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Quiz />}
       </Layout>
-    );
+    )
   }
 }
 
-export default App;
+export default App
