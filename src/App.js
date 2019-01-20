c23677912daed1dba922b01ed0954b8faff05e9b
aanarkulov <a.anarkuloff@gmail.com>
2019-01-21 02:51:50 +0600

    Пункты меню

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react'
 import Layout from './hoc/Layout/Layout'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
 import Auth from './containers/Auth/Auth'
 import QuizCreator from './containers/QuizCreator/QuizCreator'
 import QuizList from './containers/QuizList/QuizList'
+import { connect } from 'react-redux'
+import Logout from './components/Logout/Logout'
 
 class App extends Component {
+
   render() {
-    return (
-      <Layout>
+
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/" exact component={QuizList} />
+        <Redirect to="/" />
+      </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
         <Switch>
-          <Route exact path="/" component={QuizList} />
-          <Route path="/auth" component={Auth} />
           <Route path="/quiz-creator" component={QuizCreator} />
           <Route path="/quiz/:id" component={Quiz} />
+          <Route path="/" exact component={QuizList} />
+          <Route path='/logout' component={Logout} />
+          <Redirect to="/" />
         </Switch>
+      )
+    }
+    return (
+      <Layout>
+        {routes}
       </Layout>
     )
   }
 }
 
-export default App 
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: state.auth.token && true
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(App))
--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -3,15 +3,13 @@ import classes from './Drawer.css'
 import { NavLink } from 'react-router-dom'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 
-const links = [
-    { to: '/', label: 'Список тестов', exact: true },
-    { to: '/auth', label: 'Авторизация', exact: false },
-    { to: '/quiz-creator', label: 'Создать тест', exact: false }
-]
-
 class Drawer extends Component {
 
-    renderLinks() {
+    clickHandler = () => {
+        this.props.onClose()
+    }
+
+    renderLinks(links) {
         return links.map((link, index) => {
             return (
                 <li key={index}>
@@ -19,7 +17,7 @@ class Drawer extends Component {
                         to={link.to}
                         exact={link.exact}
                         activeClassName={classes.active}
-                        onClick={this.props.onClose}
+                        onClick={this.clickHandler}
                     >
                         {link.label}
                     </NavLink>
@@ -35,17 +33,25 @@ class Drawer extends Component {
             cls.push(classes.close)
         }
 
+        const links = [{ to: '/', label: 'Список', exact: true }]
+
+        if (this.props.isAuthenticated) {
+            links.push({ to: '/quiz-creator', label: 'Создать тест', exact: false })
+            links.push({ to: '/logout', label: 'Выйти', exact: false })
+        } else {
+            links.push({ to: '/auth', label: 'Авторизация', exact: false })
+        }
+
         return (
             <React.Fragment>
                 <nav className={cls.join(' ')}>
                     <ul>
-                        {this.renderLinks()}
+                        {this.renderLinks(links)}
                     </ul>
                 </nav>
-
-                {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+                {this.props.isOpen && <Backdrop onClick={this.props.onClose} />}
             </React.Fragment>
-        )
+        );
     }
 }
 
