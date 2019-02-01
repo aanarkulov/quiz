ca312e1e82c080da5757bc3bfb7d4bc1a6d37218
aanarkulov <a.anarkuloff@gmail.com>
2019-02-01 23:44:10 +0600

    setup for unit testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,17 +1,4 @@
-import { shallow, render, mount, configure } from 'enzyme'
+import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import raf from './tempPolyfills'
-
-configure({ adapter: new Adapter() })
-
-
-global.shallow = shallow
-global.render = render
-global.mount = mount
-//global.toJson = toJson
-
-// Fail tests on any warning
-console.error = message => {
-    throw new Error(message)
-}
+Enzyme.configure({ adapter: new Adapter() })
