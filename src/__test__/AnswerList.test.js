ab865f582db607185c972e72e80766b4843cbd8d
aanarkulov <a.anarkuloff@gmail.com>
2019-02-07 14:01:26 +0600

    testing AnswerList component

---
+++ src/__test__/AnswerList.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import AnswerList from '../components/ActiveQuestion/AnswerList/AnswerList'
+
+describe('AnswerList component', () => {
+
+    it('should render', () => {
+        const props = {
+            answers: [{ id: 1, text: '' }],
+            state: { 1: 'success' }
+        }
+        const wrapper = shallow(<AnswerList {...props} />)
+
+        expect(wrapper.find('AnswerItem').exists()).toBe(true)
+        expect(wrapper.find('AnswerItem').prop('state')).toEqual('success')
+    })
+
+})
--- src/components/ActiveQuestion/AnswerList/AnswerList.js
+++ src/components/ActiveQuestion/AnswerList/AnswerList.js
@@ -2,19 +2,17 @@ import React from 'react'
 import classes from './AnswerList.css'
 import AnswerItem from './AnswerItem/AnswerItem'
 
-const AnswersList = props => (
+const AnswerList = props => (
     <ul className={classes.AnswersList}>
-        {props.answers.map((answer, index) => {
-            return (
-                <AnswerItem
-                    key={index}
-                    answer={answer}
-                    onAnswerClick={props.onAnswerClick}
-                    state={props.state ? props.state[answer.id] : null}
-                />
-            )
-        })}
+        {props.answers.map((answer, index) => (
+            <AnswerItem
+                key={index}
+                answer={answer}
+                onAnswerClick={props.onAnswerClick}
+                state={props.state ? props.state[answer.id] : null}
+            />
+        ))}
     </ul>
 );
 
-export default AnswersList
+export default AnswerList
