32652e4f2dc0dfe251f6443d533a4df058f12447
aanarkulov <a.anarkuloff@gmail.com>
2019-02-07 14:05:30 +0600

    testing UI components

---
+++ src/__test__/UiComponents.test.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import Backdrop from '../components/UI/Backdrop/Backdrop'
+import Button from '../components/UI/Button/Button'
+import Input from '../components/UI/Input/Input'
+import Loader from '../components/UI/Loader/Loader'
+import Select from '../components/UI/Select/Select'
+
+describe('UI components', () => {
+
+    let wrapper, props
+
+    it('Backdrop', () => {
+        wrapper = shallow(<Backdrop />)
+        expect(wrapper)
+    })
+
+    it('Button', () => {
+        wrapper = shallow(<Button />)
+        expect(wrapper)
+    })
+
+    describe('Input', () => {
+        it('Input', () => {
+            wrapper = shallow(<Input />)
+            expect(wrapper)
+        })
+
+        it('isInvalid test', () => {
+            props = {
+                valid: false,
+                touched: true,
+                shouldValidate: { required: true, email: true }
+            }
+            wrapper = shallow(<Input {...props} />)
+
+            expect(wrapper.prop('className')).toEqual(' ')
+        })
+    })
+
+    it('Loader', () => {
+        wrapper = shallow(<Loader />)
+        expect(wrapper)
+    })
+
+    it('Select', () => {
+        props = {
+            options: [{ text: 1, value: 1 }],
+            onChange: jest.fn()
+        }
+
+        wrapper = shallow(<Select {...props} />)
+        expect(wrapper)
+    })
+
+})
+
--- src/components/UI/Select/Select.js
+++ src/components/UI/Select/Select.js
@@ -3,6 +3,7 @@ import classes from './Select.css'
 
 const Select = props => {
     const htmlFor = `${props.label}-${Math.random()}`
+
     return (
         <div className={classes.Select}>
             <label htmlFor={htmlFor}>{props.label}</label>
